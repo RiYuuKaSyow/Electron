7313ce2081bffde560d89ec3e26605b9a57c8516
Syow <crescentsyou@gmail.com>
2019-05-02 22:38:14 +0800

    gitignore Syow.exe -> Tray double-click to toggle clock finish thank for Ai chyan no FAQ

--- main.js
+++ main.js
@@ -1,33 +1,77 @@
+/*
 const electron = require('electron') ;
 const app = electron.app ;
 const browser = electron.BrowserWindow ;
+*/
 
-let mainwindow ;
+const {
+    app,
+    BrowserWindow,
+    Tray,
+    Menu,
+} = require('electron') ;
+
+app.on('ready' , createwindow) ;
+
+let win ;
+
 
 function createwindow(){
-    mainwindow = new browser({
+    let appicon = CreateTray() ;
+    win = new BrowserWindow({
         width:1280,
         height:720,
         title:"Syow"
     });
-    electron.Menu.setApplicationMenu(null) ;
-    mainwindow.webContents.openDevTools() ;
-    mainwindow.loadFile('html/time.html');
-    mainwindow.on('closed',function(){
-        mainwindow = null ;
+    Menu.setApplicationMenu(null) ;
+    
+    //win.webContents.openDevTools() ;
+    win.loadFile('./home.html');
+    win.on('minimize',function(){
+        win.hide() ;
+        
+    });
+    win.on('closed',function(){
+        win = null ;
     }) ;
+    appicon.on('double-click',function(){
+        win.isVisible()? win.hide():win.show() ;
+    });
+    
 }
+function CreateTray(){
+    
+    const contextmenu = Menu.buildFromTemplate([{
+            label:'開啟',
+            click(){
+                win.show() ;
+            }
+        },
+        {
+            label:'關閉',
+            click(){
+                win.removeAllListeners('close');
+                win.close() ;
+            }
+        }
+    ])
+    let appIcon = new Tray('./img/clock.png') ; 
+    appIcon.setToolTip('Syow') ;
+    appIcon.setHighlightMode('always') ;
+    appIcon.setContextMenu(contextmenu) ;
+    return appIcon ;
+};
 
-//mainwindow.webContents.send('MsgfromMain', 'main-process-messages show') ;
+//win.webContents.send('MsgfromMain', 'main-process-messages show') ;
 
-app.on('ready' , createwindow) ;
+
 
 app.on('window-all-closed',function(){
     app.quit() ;
 });
 
 app.on('active' , function(){
-    if( mainwindow == null ){
+    if( win == null ){
         createwindow() ;
     }
 });
